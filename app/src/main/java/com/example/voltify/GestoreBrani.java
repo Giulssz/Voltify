3a66da9cbc2c9942169597c5d45762377081e08e
Giulia <giulia.marchetti.1512@gmail.com>
2022-01-14 14:26:02 +0100

    App Voltify

---
+++ app/src/main/java/com/example/voltify/GestoreBrani.java
@@ -0,0 +1,16 @@
+package com.example.voltify;
+
+import java.util.ArrayList;
+
+public class GestoreBrani {
+
+    ArrayList<Brano> listaBrani;
+    public GestoreBrani(){
+        listaBrani = new ArrayList<Brano>();
+    }
+
+    public void addBrano(){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/voltify/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.voltify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnInserisci;
+    EditText txtTitolo;
+    GestoreBrani gb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInserisci = (Button)findViewById(R.id.btnInserisci);
+        txtTitolo = (EditText) findViewById(R.id.txtTitolo);
+
+        btnInserisci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gb.addBrano();
+            }
+        });
+    }
+}
